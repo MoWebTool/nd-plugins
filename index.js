b9d6a7844035b461e37e6e793056b33c5bab8116
crossjs <liwenfu@crossjs.com>
2015-02-09 17:42:29 +0800

    init

--- index.js
+++ index.js
@@ -5,17 +5,31 @@
 
 'use strict';
 
+/**
+ * @constructor
+ * @param {string}    name    插件名称，宿主内唯一
+ * @param {function}  starter 插件启动器，必须返回当前插件实例
+ */
 var PluginBase = function(name, starter) {
   this.name = name;
   this.starter = starter;
 };
 
+/**
+ * 供 starter 调用，一般在插件就绪时调用
+ */
 PluginBase.prototype.ready = function() {
   if (this.callback) {
     this.callback.call(this.host, this);
   }
 };
 
+/**
+ * 启动插件
+ * @param  {object}   host     宿主
+ * @param  {function} callback 插件就绪回调函数，接收两个参数：host、plugin
+ * @return {object}            当前插件实例
+ */
 PluginBase.prototype.start = function(host, callback) {
   if (host) {
     this.host = host;
@@ -25,13 +39,7 @@ PluginBase.prototype.start = function(host, callback) {
     this.callback = callback;
   }
 
-  this.plugin = this.starter(host);
-
-  if (!host.plugins) {
-    host.plugins = {};
-  }
-
-  host.plugins[this.name] = this.plugin;
+  return this.starter(host);
 };
 
 module.exports = {
@@ -41,7 +49,7 @@ module.exports = {
       this._plugins = {};
     }
 
-    this.__plugins[name] = new PluginBase(name, starter).start(this, callback).plugin;
+    this.__plugins[name] = new PluginBase(name, starter).start(this, callback);
   },
 
   getPlugin: function(name) {
